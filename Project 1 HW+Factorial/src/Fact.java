c141c8dd964394aacb80dc040c6bd538bb80aa87
Syed Sameer Ul Hasan <sameerhasan70@gmail.com>
2015-08-28 22:02:30 +0500

    First commit

---
+++ Project 1 HW+Factorial/src/Fact.java
@@ -0,0 +1,15 @@
+
+public class Fact {
+
+	public static void main(String[] args) {
+		int n=7;
+		int result=1;
+		for (int i=1;i<=n;i++){
+			result=result*i;
+		}
+		System.out.println("Factorial of 7 is " + result);
+
+
+	}
+
+}
